dfe6dcc85e435fa7908a58bc39b4e8dd62459361
JRsungjo <sharon010420@gmail.com>
2025-06-10 12:15:06 +0900

    account 후 데이터 파싱 수정 기능 4 추가

--- return.c
+++ return.c
@@ -9,6 +9,56 @@
 #define TEMP_FILE "temp_user.txt"
 //int updata_file_2(Lend_Return);
 
+void auto_borrow(Book* temp_b, char returnDate[]) {
+    bool book_integrity = true;
+    linked_list* book_list = read_book_data(&book_integrity);
+
+    bool user_integrity = true;
+    linked_list* user_list = read_user_data(&user_integrity);
+    User* user = find_by_userId(user_list, temp_b->studentId);
+
+    //대출 가능한지, 사용자 연체여부, 대출 가능 도서 개수 확인
+    if (user->isOverdue == 'N' && user->lendAvailable > 0) {
+        //해당 사용자의 <대출 중인 도서 BID 목록>에 도서 BID 추가, <대출 가능 도서 개수>, <예약 가능 도서 개수>를 수정
+        strcpy(user->lentBids[5 - user->lendAvailable], temp_b->bid);
+        user->lendAvailable--;
+        user->reserveAvailable++;
+        update_file(USER_FILE, user_list);
+
+        //도서 정보 파일 내 해당 도서의 <대출 가능 여부>를 ‘N’, <예약 가능 여부>는 ‘Y'로 수정, <예약중인 사용자 학번>을 삭제
+        temp_b->isAvailable = 'N';
+        temp_b->isReserveAvailable = 'Y';
+        strcpy(temp_b->studentId, "");
+        update_file(BOOK_FILE, book_list);
+
+        //대출 및 반납 기록 파일에서 해당 사용자의 ID, 입력한 BID, 대출 날짜(대출 날짜는 전 이용자의 반납날짜로 저장), 반납 날짜(대출 중이므로 미반납 상태인 ‘ - ’ 저장)순으로 저장
+        bool lend_integrity = true;
+        linked_list* lend_list = read_borrow_data(&lend_integrity);
+        Lend_Return* new_lend = (Lend_Return*)malloc(sizeof(Lend_Return));
+
+        strcpy(new_lend->userid, user->studentId);
+        strcpy(new_lend->bookBid, temp_b->bid);
+        strcpy(new_lend->borrowDate, returnDate);
+        strcpy(new_lend->returnDate, "");
+
+        insert_back(lend_list, new_lend);
+        update_file(LEND_RETURN_FILE, lend_list);
+        free(new_lend);
+        //무결성 처리??
+    }
+    else {
+        //도서 정보 파일 내 해당 도서의 <예약 가능 여부>를 ‘Y’로 바꾸고 <예약 중인 사용자 학번 > 을 삭제하며 예약이 취소됩니다.
+        temp_b->isReserveAvailable = 'Y';
+        strcpy(temp_b->studentId, "");
+
+        update_file(BOOK_FILE, book_list);
+    }
+
+    return;
+}
+
+
+
 void run_return() {
     if (!is_logged_in) {
         printf("You must login first to return books.\n");
@@ -137,12 +187,16 @@ void run_return() {
     fgets(confirm, sizeof(confirm), stdin);
     trim(confirm);
 
-    if (_stricmp(confirm, "No") == 0) {
+    if (check_input(confirm) == 0) {
         printf("Returning cancelled.\n");
         return;
     }
-    temp_u->lendAvailable++;
-    temp_b->isAvailable = 'Y';
+    else {
+        temp_u->lendAvailable++;
+        temp_b->isAvailable = 'Y';
+        if (temp_u->reserveAvailable == 'N')
+            auto_borrow(temp_b, return_date);
+    }
 
     if (!update_file(USER_FILE, user_list) || !update_file(BOOK_FILE, book_list) || !update_file(LEND_RETURN_FILE, lend_list)) {
         return;
